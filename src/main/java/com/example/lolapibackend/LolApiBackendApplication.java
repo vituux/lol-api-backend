1f2f3caaccb1d05c469a14f7c62acbdbc4486878
Victor Santos <victor20ve@hotmail.com>
2021-08-05 11:38:23 -0300

    Config inicial do backend

---
+++ src/main/java/com/example/lolapibackend/LolApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lolapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolApiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lolapibackend/LolApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lolapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
